26c143af741200dbcc7a330ace3f739ffb7dcc68
andrew.borg <andrewbrg87@gmail.com>
2024-01-28 21:21:36 +0100

    docs(core): Add generics

--- src/services/evaluator.ts
+++ src/services/evaluator.ts
@@ -13,18 +13,18 @@ export class Evaluator {
    * @param rule The rule to evaluate.
    * @param criteria The criteria to evaluate the rule against.
    */
-  evaluate<T>(rule: Rule, criteria: object | object[]): boolean | any {
+  evaluate<T>(rule: Rule, criteria: object | object[]): T | boolean {
     // Cater for the case where the conditions property is not an array.
     const conditions =
       rule.conditions instanceof Array ? rule.conditions : [rule.conditions];
 
     if (criteria instanceof Array) {
-      const result = [];
+      const result: T | boolean[] = [];
       for (const c of criteria) {
-        result.push(this.evaluateRule<T>(conditions, c, rule?.default));
+        result.push(this.evaluateRule(conditions, c, rule?.default));
       }
 
-      return result;
+      return result as T | boolean;
     }
 
     return this.evaluateRule<T>(conditions, criteria, rule?.default);
